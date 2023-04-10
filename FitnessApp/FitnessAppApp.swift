a3283f75385066d8f341bea07ec9e1e68e225abb
Adrian Rodzic <adrianrodzic33@gmail.com>
2023-04-10 17:38:31 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Adrian Rodzic on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
